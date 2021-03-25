bfbff4a1a68c24d5f85e2bd578575dbcc6f4af47
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2021-03-25 09:28:21 +0100

    Add files via upload

--- Button.gd
+++ Button.gd
@@ -1,5 +1,7 @@
 extends Control
 
+# Collect buttons
+
 func _ready():
 	print(Global.save)
 	if not Global.save.get(Global.current_scene_name):
@@ -11,6 +13,10 @@ func _ready():
 func _on_button_down():
 	if not Global.save.get(Global.current_scene_name):
 		Global.save[Global.current_scene_name] = {}
+		
+	#Add button name to save
+	#Save		world name				  node name		just a value
+	#save[level_name][node_name] = true
 	Global.save[Global.current_scene_name][name] = true
 	Global.save_data()
 	Global.add_points()
